cec04aaa7a81424801a577f335b9f996b1fe0a6b
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-05-28 15:57:09 +0900

    프로젝트 기본 설정 : init : https://github.com/Cassiiopeia/suh-api-log/issues/1

---
+++ src/main/java/me/suhsaechan/suhapilog/SuhApiLogApplication.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhapilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhApiLogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SuhApiLogApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/suhsaechan/suhapilog/SuhApiLogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhapilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhApiLogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
